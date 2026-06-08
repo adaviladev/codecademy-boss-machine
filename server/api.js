9540eefe483de3cdf81554280a6a6bb9e42c380f
adaviladev <alvarodaviladev@gmail.com>
2026-06-08 19:50:34 -0400

    feat: add router files for minions, ideas and meetings

--- server/api.js
+++ server/api.js
@@ -1,6 +1,10 @@
-const express = require('express');
+const express = require("express");
 const apiRouter = express.Router();
 
+const ideasRouter = require("./ideas");
+const meetingsRouter = require("./meetings");
+const minionsRouter = require("./minions");
 
+apiRouter.use("/ideas", ideasRouter);
 
 module.exports = apiRouter;
