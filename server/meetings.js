93cb8745540583eb6f41e992f4ea803e0416b35e
adaviladev <alvarodaviladev@gmail.com>
2026-06-10 19:06:04 -0400

    feat: complete meetings routes

--- server/meetings.js
+++ server/meetings.js
@@ -1,4 +1,25 @@
 const express = require("express");
 const meetingsRouter = express.Router();
+const {
+  getAllFromDatabase,
+  createMeeting,
+  addToDatabase,
+  deleteAllFromDatabase,
+} = require("./db");
+
+meetingsRouter.get("/", (req, res) => {
+  res.send(getAllFromDatabase("meetings"));
+});
+
+meetingsRouter.post("/", (req, res) => {
+  const newMeeting = createMeeting();
+  const savedMeeting = addToDatabase("meetings", newMeeting);
+  res.status(201).send(savedMeeting);
+});
+
+meetingsRouter.delete("/", (req, res) => {
+  deleteAllFromDatabase("meetings");
+  res.status(204).send();
+});
 
 module.exports = meetingsRouter;
