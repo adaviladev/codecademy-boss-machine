ef71c1854909d46f73a6c8d69a737f585da3bd0b
adaviladev <alvarodaviladev@gmail.com>
2026-06-15 01:08:40 -0400

    feat: complete minion work routes

--- server/minions.js
+++ server/minions.js
@@ -21,8 +21,50 @@ minionsRouter.get("/:minionId", (req, res) => {
   }
 });
 
+minionsRouter.get("/:minionId/work", (req, res) => {
+  const minion = getFromDatabaseById("minions", req.params.minionId);
+  if (!minion) {
+    res.status(404).send();
+  } else {
+    const allWork = getAllFromDatabase("work");
+    const minionWork = allWork.filter((work) => {
+      return work.minionId === req.params.minionId;
+    });
+    res.send(minionWork);
+  }
+});
+
+minionsRouter.post("/:minionId/work", (req, res) => {
+  const minion = getFromDatabaseById("minions", req.params.minionId);
+  if (!minion) {
+    res.status(404).send();
+  } else {
+    req.body.minionId = req.params.minionId;
+    const newWork = addToDatabase("work", req.body);
+    res.status(201).send(newWork);
+  }
+});
+
 minionsRouter.post("/", (req, res) => {
   res.status(201).send(addToDatabase("minions", req.body));
+});
+
+minionsRouter.put("/:minionId/work/:workId", (req, res) => {
+  const minion = getFromDatabaseById("minions", req.params.minionId);
+  if (!minion) {
+    return res.status(404).send();
+  }
+  const work = getFromDatabaseById("work", req.params.workId);
+  if (!work) {
+    return res.status(404).send();
+  }
+  if (work.minionId !== req.params.minionId) {
+    return res.status(400).send();
+  }
+  req.body.id = req.params.workId;
+  req.body.minionId = req.params.minionId;
+  const updatedWork = updateInstanceInDatabase("work", req.body);
+  res.send(updatedWork);
 });
 
 minionsRouter.put("/:minionId", (req, res) => {
@@ -32,6 +74,22 @@ minionsRouter.put("/:minionId", (req, res) => {
   } else {
     res.send(minion);
   }
+});
+
+minionsRouter.delete("/:minionId/work/:workId", (req, res) => {
+  const minion = getFromDatabaseById("minions", req.params.minionId);
+  if (!minion) {
+    return res.status(404).send();
+  }
+  const work = getFromDatabaseById("work", req.params.workId);
+  if (!work) {
+    return res.status(404).send();
+  }
+  if (work.minionId !== req.params.minionId) {
+    return res.status(400).send();
+  }
+  deleteFromDatabasebyId("work", req.params.workId);
+  res.sendStatus(204);
 });
 
 minionsRouter.delete("/:minionId", (req, res) => {
